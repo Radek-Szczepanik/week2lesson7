47028e6d0e4191e8259dec38f2a96e1f7cc7604b
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2020-08-07 17:34:56 +0200

    update file Zadanie10

--- Zadanie10/Program.cs
+++ Zadanie10/Program.cs
@@ -6,7 +6,45 @@ namespace Zadanie10
     {
         static void Main(string[] args)
         {
-            
+            // deklaracja zmiennych
+
+            int sideA, sideB, sideC;
+
+            // przypisanie do zmiennych
+
+            for(; ; )
+            {
+                Console.WriteLine("Podaj długość boku a:");
+                string A = Console.ReadLine();
+                int.TryParse(A, out sideA);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Podaj długość boku b:");
+                string B = Console.ReadLine();
+                int.TryParse(B, out sideB);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Podaj długość boku c:");
+                string C = Console.ReadLine();
+                int.TryParse(C, out sideC);
+
+                Console.WriteLine();
+
+                // warunek, który sprawdza czy można zbudować trójkąt
+
+                if (sideA > 0 && sideB > 0 && sideC > 0 && sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+                {
+                    Console.WriteLine("Można zbudować trójkąt");
+                }
+                else
+                {
+                    Console.WriteLine("Nie można zbudować trójkąta");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
